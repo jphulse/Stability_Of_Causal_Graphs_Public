83c15c11d1dd40cdbcb43154c30ba8675c983875
jphulse <162621611+jphulse@users.noreply.github.com>
2024-04-25 11:26:16 -0400

    Update ant_alpha_test.R

--- R_Scripts/ant_alpha_test.R
+++ R_Scripts/ant_alpha_test.R
@@ -1,3 +1,4 @@
+# Experiment testing on Ant with a shifting alpha, the number of iterations can be changed to make this test as small or large as one wants by adjusting the bounds of the outer for loop
 library(pcalg)
 library(dplyr)
 data("ant-1.3")
